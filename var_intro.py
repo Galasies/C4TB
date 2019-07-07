a0d18d0ce3ec69e508c898dbb3450232814e4601
Galasies <chipulove17@gmail.com>
2019-07-07 14:48:15 +0700

    First commit

---
+++ var_intro.py
@@ -0,0 +1,67 @@
+
+#a = input()
+#b = input()
+#print(a+b)
+# a = int(input())
+# b = int(2019)
+# sum = b - a
+# print(sum)
+
+
+
+# def print_helloworld(, b):
+# #     multiply=a*b
+# #     return multiply
+# # a = 1
+# # b = 2
+# # print(print_helloworld (a ,b))
+
+#     print(input_a)
+#     print(input_b)
+#     a = 3   
+#     return a
+
+# a = "1"
+# b = "2"
+# c = print_helloworld(a, b)
+# print(c)
+# print(a)
+
+
+
+
+
+#cách ??
+# number1 = int (3)
+# square1 = number1**2
+# print(square1)
+
+# number2 = int (1)
+# square2 = number2**2
+# print(square2)
+
+# c = int(square1 + square2)
+# print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def print_helloworld(input_a, input_b):
+    c = int(input_a) + int(input_b)
+    return c
+
+a = input()
+b = input()
+c = print_helloworld(a, b)
+print (c)
